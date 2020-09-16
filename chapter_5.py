096d9ac18cc97369635be62592897b2a93979a68
krushade <shade.jdm@yandex.ru>
2020-09-16 10:03:34 +0300

    Add exercise_1

---
+++ chapter_5.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+#lesson
+    cars = ['honda', 'subaru', 'bmw', 'toyota', 'mazda']
+    for car in cars:
+        if car == 'bmw':
+            print(car.upper())
+        else:
+            print(car.title())
---
+++ exercise_1.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    all_nums = [num for num in range(100, 1001)]
+    even_nums = []
+    def even():
+        for even_num in all_nums:
+            if even_num % 2 == 0:
+                even_nums.append(even_num)
+        print(even_nums)
+    print(even())
