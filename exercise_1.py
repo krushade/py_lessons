800132ff292022e4c0cb4480c1daa62a7a129e1f
krushade <shade.jdm@yandex.ru>
2020-09-16 11:12:21 +0300

    Exercise_1 correction

--- exercise_1.py
+++ exercise_1.py
@@ -1,9 +1,10 @@
-if __name__ == '__main__':
+if __name__ == "__main__":
     all_nums = [num for num in range(100, 1001)]
-    even_nums = []
-    def even():
-        for even_num in all_nums:
+    def even(numbers):
+        even_nums = []
+        for even_num in numbers:
             if even_num % 2 == 0:
                 even_nums.append(even_num)
+        even_nums.reverse()
         return even_nums
-    print(even())
+    print(even(all_nums))
