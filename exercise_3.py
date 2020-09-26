20f77b6db5cc616fcc42e202b9aeeaf13ccf02b2
krushade <shade.jdm@yandex.ru>
2020-09-26 12:47:13 +0300

    Exe_3, add def

--- exercise_3.py
+++ exercise_3.py
@@ -1,7 +1,10 @@
 numbers = [1, 21, 51, 28, 11, 32, 35, 21, 15, 1, 11, 32, 78, 2, 21, 51, 38, 14, 21, 1, 21, 1, 35]
-nums_sorted = {}
-for num in numbers:
-    nums_sorted[num] = numbers.count(num)
-
+numbers_2 = [1, 2, 3, 4, 4, 4, 5, 6, 6, 6, 6, 6, 1, 1, 1, 5]
+def sort(nums):
+    nums_sorted = {}
+    for  num in nums:
+        nums_sorted[num] = nums.count(num)
+    return  nums_sorted
 if __name__ == '__main__':
-    print(nums_sorted)
+    print(sort(numbers))
+    print(sort(numbers_2))
