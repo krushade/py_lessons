1316e2e3c6488ed9bee39759e8eb618056ad330f
krushade <shade.jdm@yandex.ru>
2020-09-26 17:33:21 +0300

    Add exercise_4

--- exercise_4/exercise_4.py
+++ exercise_4/exercise_4.py
@@ -11,16 +11,15 @@ for x in list_dir:
 
 if __name__ == '__main__':
 
-
     def sh(files):
         for line in files:
             splt = line.splitlines()
-            for x in splt:
-                sh_str = x.split('/')
-                for x in sh_str:
-                    if x.endswith('.sh'):
+            for s in splt:
+                sh_str = s.split('/')
+                for t in sh_str:
+                    if t.endswith('.sh'):
                         with open('sorted_sh.txt', 'a') as sorted_files:
-                            sorted_files.write(f"{x}\n")
+                            sorted_files.write(f"{t}\n")
 
 
     for job_file in sort_list:
